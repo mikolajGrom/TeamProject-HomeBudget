fe4255410f76e8b5cdff10e99f4bdceec74d51e3
mikolajGrom <mikolaj.gromadzik@gamil.com>
2019-06-12 00:10:50 +0200

    final version of project

---
+++ HomeBudget/Controllers/HistoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeBudget.Controllers
+{
+    public class HistoryController : Controller
+    {
+        
+        public ActionResult HistoryPage()
+        {
+            ViewBag.Message = "The list of purchases.";
+
+            return View();
+        }
+    }
+}
